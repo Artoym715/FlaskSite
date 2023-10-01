799d228eb987743adee93b05338916f0c76f15c6
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2023-10-01 20:43:09 +0600

    Страница 404

--- main.py
+++ main.py
@@ -3,31 +3,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 menu = [
-        {"name": "Главная", "url": "/"},
-        {"name": "Блог", "url": "blog"},
-        {"name": "Магазин", "url": "catalog"},
-        {"name": "О сайте", "url": "about"}
-       ]
+    {"name": "Главная", "url": "/"},
+    {"name": "Магазин", "url": "catalog"},
+    {"name": "О сайте", "url": "about"}
+]
 
 
 @app.route("/")
 def index():
-    return render_template('Index.html', title='Главная', menu=menu)
-
-
-@app.route("/blog")
-def blog():
-    return render_template('blog.html', title='Блог', menu=menu)
+    return render_template('shop/Index.html', title='Главная', menu=menu)
 
 
 @app.route("/catalog")
 def catalog():
-    return render_template('catalog.html', title='Магазин', menu=menu)
+    return render_template('shop/catalog.html', title='Магазин', menu=menu)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='О сайте', menu=menu)
+    return render_template('shop/about.html', title='О сайте', menu=menu)
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('shop/page404.html', title='Страница не найдена!', menu=menu)
 
 
 if __name__ == "__main__":
