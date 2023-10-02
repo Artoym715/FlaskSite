556394472b207d6f13169d9652a2b45164c85724
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2023-10-03 02:00:44 +0600

    Шаблон продукта

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 menu = [
     {"name": "Главная", "url": "/"},
-    {"name": "Магазин", "url": "/catalog"},
+    {"name": "Магазин", "url": "/catalog/clothes"},
     {"name": "О сайте", "url": "/about"}
 ]
 
@@ -14,13 +14,14 @@ def index():
     return render_template('shop/Index.html', title='Главная', menu=menu)
 
 
-@app.route("/catalog")
-def catalog():
+@app.route("/catalog/<name>")
+def catalog(name):
     context = {
         'category':
             [
                 {
                     'id': 1,
+                    'nameCategory': 'clothes',
                     'name': 'ОДЕЖДА',
                 }
             ]
@@ -28,30 +29,39 @@ def catalog():
     return render_template('shop/catalog.html', title='Магазин', menu=menu, context=context)
 
 
-@app.route("/catalog/category/")
-def category():
+@app.route("/catalog/<name>/category")
+def category(name):
     context = {
-        'category':
+        'Posts':
             [
                 {
                     'id': 1,
+                    'nameCategory': 'clothes',
                     'name': 'КУРТКА',
                     'img': '/static//img/s-l1600.jpg'
 
                 },
                 {
                     'id': 2,
+                    'nameCategory': 'clothes',
                     'name': 'БРЮКИ',
                     'img': '/static//img/bryuki-chinosy-regular-fit-straight-chernyj.jpg'
                 },
                 {
                     'id': 3,
+                    'nameCategory': 'clothes',
                     'name': 'ОБУВЬ',
                     'img': '/static//img/png-clipart-shoe-leather-adidas-puma-men-s-big-shoes-png-material-brown.png'
                 }
             ]
     }
     return render_template('shop/category.html', title='Одежда', menu=menu, context=context)
+
+
+@app.route("/catalog/<name>/category/product/<int:id>")
+def product(name, id):
+
+    return render_template('shop/product.html', title='Одежда', menu=menu, id=id)
 
 
 @app.route("/about")
