aea5f91d7b0f9e707ecf7e3cc722ea5c598446e1
Dmitry Bystrov <98824571+Artoym715@users.noreply.github.com>
2023-10-02 21:59:43 +0600

    Fix menu

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 menu = [
     {"name": "Главная", "url": "/"},
-    {"name": "Магазин", "url": "catalog"},
-    {"name": "О сайте", "url": "about"}
+    {"name": "Магазин", "url": "/catalog"},
+    {"name": "О сайте", "url": "/about"}
 ]
 
 
@@ -16,7 +16,42 @@ def index():
 
 @app.route("/catalog")
 def catalog():
-    return render_template('shop/catalog.html', title='Магазин', menu=menu)
+    context = {
+        'category':
+            [
+                {
+                    'id': 1,
+                    'name': 'ОДЕЖДА',
+                }
+            ]
+    }
+    return render_template('shop/catalog.html', title='Магазин', menu=menu, context=context)
+
+
+@app.route("/catalog/category/")
+def category():
+    context = {
+        'category':
+            [
+                {
+                    'id': 1,
+                    'name': 'КУРТКА',
+                    'img': '/static//img/s-l1600.jpg'
+
+                },
+                {
+                    'id': 2,
+                    'name': 'БРЮКИ',
+                    'img': '/static//img/bryuki-chinosy-regular-fit-straight-chernyj.jpg'
+                },
+                {
+                    'id': 3,
+                    'name': 'ОБУВЬ',
+                    'img': '/static//img/png-clipart-shoe-leather-adidas-puma-men-s-big-shoes-png-material-brown.png'
+                }
+            ]
+    }
+    return render_template('shop/category.html', title='link.name', menu=menu, context=context)
 
 
 @app.route("/about")
@@ -26,7 +61,7 @@ def about():
 
 @app.errorhandler(404)
 def pageNotFound(error):
-    return render_template('shop/page404.html', title='Страница не найдена!', menu=menu)
+    return render_template('shop/page404.html', title='Страница не найдена!', menu=menu), 404
 
 
 if __name__ == "__main__":
